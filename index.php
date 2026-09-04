8cfcb2154aab948f930f513799a9aa5c854304a4
damiant-ui <tarraste.damian@gmail.com>
2026-09-04 09:58:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,5 +7,16 @@ $test = null;
 $test = [a, true, "Fark"];
 $test = new stdClass();
 $test->name = "Damian";
+$test = 10 + 10;
+$test = 2 * 3;
+$test = 9 / 3; //Calculating works
+$test = "Heloo" . " World" . "!";  // use . to plus texts
+$test .= "!!!!!!"; // = $test . "!!!!!!"
+$name = "Damian";
+$age = 33;
+$test = $name . " is " . $age . " years old";
+$test = "$name is $age years old";
+$test = "Hello\n\nWorld;"
+
 var_dump($test);
 ?>
