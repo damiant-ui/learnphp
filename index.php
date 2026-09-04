960ec8584a7b3113a1f407fa09ab04aba20c8344
damiant-ui <tarraste.damian@gmail.com>
2026-09-04 09:26:50 +0300

    index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-echo "Hello,World!"
+$test = "hello";
+$test = "3.1";
+$test = true;
+$test = false;
+$test = null;
+$test = [a, true, "Fark"];
+$test = new stdClass();
+$test->name = "Damian";
+var_dump($test);
 ?>
