eaafc887c5a268f40e249732aa15fdcd1c426e04
Damian Tarraste <tarraste.damian@gmail.com>
2026-09-09 11:38:29 +0300

    index.php

--- index.php
+++ index.php
@@ -1,22 +1,26 @@
 <?php
-$test = "hello";
-$test = "3.1";
-$test = true;
-$test = false;
-$test = null;
-$test = [a, true, "Fark"];
-$test = new stdClass();
-$test->name = "Damian";
-$test = 10 + 10;
-$test = 2 * 3;
-$test = 9 / 3; //Calculating works
-$test = "Heloo" . " World" . "!";  // use . to plus texts
-$test .= "!!!!!!"; // = $test . "!!!!!!"
-$name = "Damian";
-$age = 33;
-$test = $name . " is " . $age . " years old";
-$test = "$name is $age years old";
-$test = "Hello\n\nWorld;"
+$numbers = array(1,2,3);
+$numbers = [1,2,3];
+var_dump($numbers[1]);
+$numbers[2] = 5;
+var_dump($numbers);
+$test = [
+    'name' => 'Damian',
+    'age' => 19,
+    1,
+    2,
+    3,
+    'color' => 'red',
+    'music' => 'rock',
+    100 => 6,
+    7,
+];
+var_dump($text);
+var_dump($text['name']);
 
+$combined = [1,2,3] + [4,5,6,7];
+var_dump($combined);
+array_push($test, 67, 'asd', 322);
+unset($test[100]);
 var_dump($test);
 ?>
